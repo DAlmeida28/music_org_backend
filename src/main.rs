c99034c95afeff9143666316c1cd98276b637ab9
Daniel Almeida <almeida.danny@gmail.com>
2025-10-14 19:03:32 -0500

    Add CORS, updated Json response to include genre id, modified use, add genre struct for new query macro

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-use axum::http::StatusCode;
-use axum::{Json, Router, debug_handler, extract::State, response::IntoResponse, routing::get};
+use axum::{
+    Json, Router,
+    extract::State,
+    http::{Method, StatusCode},
+    response::IntoResponse,
+    routing::get,
+};
+use serde::Serialize;
 use sqlx::PgPool;
-use std::env;
-use std::error::Error;
-use std::net::SocketAddr;
+use std::{env, error::Error, net::SocketAddr};
+use tower_http::cors::CorsLayer;
+
+#[derive(Serialize)]
+struct Genre {
+    id: String,
+    name: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
+
+    let allowed_origins = ["http://localhost:5173".parse().unwrap()];
+
+    let cors = CorsLayer::new()
+        .allow_origin(allowed_origins)
+        .allow_methods(vec![Method::GET]);
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is missing");
 
@@ -15,26 +32,29 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let app = Router::new()
         .route("/genre", get(get_genre).post(get_genre))
+        .layer(cors)
         .with_state(pool.clone());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
-    println!("Listening on : {}", addr);
+
+    println!("Running server on {}", addr);
+
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, app).await?;
 
     Ok(())
 }
 
-#[debug_handler]
-async fn get_genre(State(pool): State<PgPool>) -> Result<impl IntoResponse, (StatusCode, String)> {
-    match sqlx::query_scalar::<_, String>("SELECT name FROM genre")
+async fn get_genre(State(pool): State<PgPool>) -> impl IntoResponse {
+    match sqlx::query_as!(Genre, "SELECT id, name FROM genre")
         .fetch_all(&pool)
         .await
     {
-        Ok(genres) => Ok(Json(genres)),
-        Err(_) => Err((
+        Ok(genres) => Json(genres).into_response(),
+        Err(err_msg) => (
             StatusCode::INTERNAL_SERVER_ERROR,
-            "Database query failed".to_string(),
-        )),
+            format!("Database query failed: {}", err_msg),
+        )
+            .into_response(),
     }
 }
